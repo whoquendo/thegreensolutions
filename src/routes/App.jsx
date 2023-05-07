c74e653d1523f456b103fad6bae8f24ffdccb158
whoquendo <whoquendo@gmail.com>
2023-05-07 21:47:31 +0200

    [ADD] Creacion archivo htaccess para solucionar problema con produccion

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as  Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from '../containers/Layout';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
